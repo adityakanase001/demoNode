d0d511330dced48b255e96865c33fd6b9180946e
RohitGhorpade150397 <rohitghorpade78@gmail.com>
2019-12-19 21:56:13 +0530

    this is my first commi

---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+var express =  require("express");
+var adminRouter = express();
+
+adminRouter.get("/", (request, response)=>
+{
+    response.send("This is Admin Page");
+});
+
+adminRouter.get("/:ANo", (request, response)=>
+{
+    response.send("You searched for Admin No " + request.params.ANo);
+    ///fgrtgrtjgntng
+});
+
+module.exports = adminRouter;
